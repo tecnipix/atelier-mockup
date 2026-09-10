595bdb2e9cd1ecbaf1fdf5ea2235105e9529ebef
tecnipix <nicolastecles@gmail.com>
2026-09-10 17:31:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'atelier-mockup-v5';
+const CACHE_NAME = 'atelier-mockup-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
